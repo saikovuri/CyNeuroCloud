8a300d0a184691ad047a0ef71f7358f3e7d8bf19
saikovuri <saikovuri367@gmail.com>
2017-11-04 22:58:11 -0500

    Modified index page

--- script.js
+++ script.js
@@ -28,6 +28,32 @@
 	});
 
 
+$(document).ready(function(){
+
+		$('input:image[name=image1]').click(function(){
+			if($(this).val() == 1){
+				document.getElementById("selection1").style.display = "block";
+			}else{
+				document.getElementById("selection1").style.display = "none";
+			}
+
+			if($(this).val() == 2){
+				document.getElementById("selection2").style.display = "block";
+			}else{
+				document.getElementById("selection2").style.display = "none";
+			}
+			if($(this).val() == 3){
+				document.getElementById("selection3").style.display = "block";
+			}else{
+				document.getElementById("selection3").style.display = "none";
+			}
+
+		
+		});
+	});
+
+
+
 	$(function () {
     $("#fmri").on("click", function () {
         $("#main").load("fmri.php");
