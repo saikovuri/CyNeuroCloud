b4cbd6f3ef87a178fd43967c88fc7c64be25bd18
Sue Brownawell <github@brownawell.us>
2017-12-06 09:58:13 -0600

    Revert db configurations to norm

--- CyNeuroLaravel/config/database.php
+++ CyNeuroLaravel/config/database.php
@@ -42,8 +42,8 @@ return [
         'mysql' => [
             'driver' => 'mysql',
             'host' => env('DB_HOST', '127.0.0.1'),
-            'port' => env('DB_PORT', '3308'),
-            'database' => env('DB_DATABASE', 'cyneuro'),
+            'port' => env('DB_PORT', '3306'),
+            'database' => env('DB_DATABASE', 'mysql'),
             'username' => env('DB_USERNAME', 'root'),
             'password' => env('DB_PASSWORD', ''),
             'charset' => 'utf8mb4',
